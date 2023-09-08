6cf25a96e2c40eef097b4f15e393bddcbd7773af
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2023-09-08 12:08:23 -0400

    Don't die to exceptions in pythonrc
    Unhandled exceptions here prevent me from opening a repl

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,2 +1,6 @@
-from gptalk.utils import fetch_url, summarize
-from readability import Document
+# flake8: noqa
+try:
+    from gptalk.utils import fetch_url, summarize
+    from readability import Document
+except Exception as exc:
+    print(exc)
