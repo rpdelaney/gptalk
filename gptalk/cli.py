e7f9ac2beffedd85bf1bc8bded05b8a9f60ce12c
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2024-11-23 11:27:39 -0500

    Add explain command

--- gptalk/cli.py
+++ gptalk/cli.py
@@ -133,3 +133,19 @@ def translate() -> NoReturn:
     )
 
     sys.exit(0)
+
+
+@cli.command()
+def explain() -> NoReturn:
+    """Explain some code."""
+    data = get_input()
+
+    print(
+        talk(
+            prompt_system=prompts.explain,
+            data_user=data,
+            model=GPT_MODEL_DEFAULT,
+        )
+    )
+
+    sys.exit(0)
